c339a62d691ee46ad0a09bbe9d20d7ac5d82e220
Ethan Husband <eth.husband@gmail.com>
2024-12-06 18:19:20 +1100

    Add cancel method and fireNow option

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
+export type DebounceOptions = {
+  fireNow?: boolean
+}
+
 class SignatureDebouncer {
   private uniqueRuns: Record<string, NodeJS.Timeout> = {};
 
-  public run(func: () => any, signature: Object = {}, duration = 1000) {
+  /**
+   * Debounce a function invocation, based on a provided signature and timeout duration.
+   * 
+   * @param func the function to debounce
+   * @param signature the signature the function debounces based off
+   * @param duration the duration of the debounce in ms
+   * @param options additional params
+   */
+  public run(func: () => void, signature: Object, duration: number, options: DebounceOptions = {}) {
     const encodedSignature = this.encodeSignature(signature);
-    const timeout = this.uniqueRuns[encodedSignature];
+    const existingTimeout = this.uniqueRuns[encodedSignature];
 
     // Reset the function timer due to untimely invocation
-    if (timeout) {
-      clearTimeout(timeout);
+    if (existingTimeout) {
+      clearTimeout(existingTimeout);
+    }
+
+    if (options.fireNow) {
+      func();
+      return;
     }
 
     this.uniqueRuns[encodedSignature] = setTimeout(() => {
@@ -16,8 +33,20 @@ class SignatureDebouncer {
     }, duration);
   }
 
-  private cleanupRun(encodedSignature: any) {
+  /**
+   * Cancel any pending invocation for a provided debounce signature.
+   * 
+   * @param signature the signature to cancel pending invocations for
+   */
+  public cancel(signature: Object) {
+    const encodedSignature = this.encodeSignature(signature);
+
+    this.cleanupRun(encodedSignature);
+  }
+
+  private cleanupRun(encodedSignature: string) {
     let timeout = this.uniqueRuns[encodedSignature];
+
     if (timeout) {
       clearTimeout(timeout);
       delete this.uniqueRuns[encodedSignature];
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,69 +1,95 @@
 import debouncer from "../src/index";
 
-const sleep = (ms: number = 1000) => new Promise((r) => setTimeout(r, ms));
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+const signature = "this is an example of a signature, it is arbitrary data!";
+const duration = 1000;
 
 describe("Signature Debouncer", () => {
   it("invokes function only after timeout", async () => {
-    const exampleArray: string[] = [];
-    const exampleFunc = () => exampleArray.push("Test!");
+    const array: string[] = [];
+    const func = () => array.push("Test!");
 
-    debouncer.run(exampleFunc);
-    expect(exampleArray).toHaveLength(0);
-    await sleep();
+    debouncer.run(func, signature, duration);
+    expect(array).toEqual([]);
 
-    expect(exampleArray).toHaveLength(1);
+    await sleep(duration);
+    expect(array).toEqual(["Test!"]);
   });
 
-  it("clears previous invocations", async () => {
-    const exampleArray: string[] = [];
-    const exampleFunc = () => exampleArray.push("Test!");
+  it("clears previous invocations once executed", async () => {
+    const array: string[] = [];
+    const func = () => array.push("Test!");
 
-    debouncer.run(exampleFunc);
-    expect(exampleArray).toHaveLength(0);
+    debouncer.run(func, signature, duration);
+    expect(array).toEqual([]);
 
-    await sleep();
-    expect(exampleArray).toHaveLength(1);
+    await sleep(duration);
+    expect(array).toEqual(["Test!"]);
 
-    debouncer.run(exampleFunc);
-    await sleep();
-    expect(exampleArray).toHaveLength(2);
+    debouncer.run(func, signature, duration);
+    await sleep(duration);
+    expect(array).toEqual(["Test!", "Test!"]);
   });
 
-  it("prevents multiple invocations within timeout", async () => {
-    const exampleArray: string[] = [];
-    const exampleFunc = () => exampleArray.push("Test!");
+  it("debounces multiple invocations within the timeout duration", async () => {
+    const array: string[] = [];
+    const func = () => array.push("Test!");
 
-    debouncer.run(exampleFunc);
-    debouncer.run(exampleFunc);
-    debouncer.run(exampleFunc);
-    expect(exampleArray).toHaveLength(0);
+    debouncer.run(func, signature, duration);
+    debouncer.run(func, signature, duration);
+    debouncer.run(func, signature, duration);
+    expect(array).toEqual([]);
 
-    await sleep();
-    expect(exampleArray).toHaveLength(1);
+    await sleep(duration);
+    expect(array).toEqual(["Test!"]);
   });
 
   it("treats different signatures as seperate invocations", async () => {
-    const exampleArray: string[] = [];
-    const exampleFunc = () => exampleArray.push("Test!");
+    const array: string[] = [];
+    const func = () => array.push("Test!");
 
-    debouncer.run(exampleFunc);
-    debouncer.run(exampleFunc, { key: "value" });
-    expect(exampleArray).toHaveLength(0);
+    debouncer.run(func, signature, duration);
+    debouncer.run(func, { key: "this is a different signature" }, duration);
+    expect(array).toEqual([]);
 
-    await sleep();
-    expect(exampleArray).toHaveLength(2);
+    await sleep(duration);
+    expect(array).toEqual(["Test!", "Test!"]);
   });
 
-  it("works independently of the function applied", async () => {
-    const exampleArray: string[] = [];
-    const exampleFunc = () => exampleArray.push("Test!");
-    const anotherFunc = () => exampleArray.push("Test Again???");
+  it("debounces based on the signature and not the function", async () => {
+    const array: string[] = [];
+    const func = () => array.push("Test!");
+    const anotherFunc = () => array.push("Test Again???");
 
-    debouncer.run(exampleFunc, { same: "signature" });
-    debouncer.run(anotherFunc, { same: "signature" });
-    expect(exampleArray).toHaveLength(0);
+    debouncer.run(func, signature, duration);
+    debouncer.run(anotherFunc, signature, duration);
+    expect(array).toEqual([]);
 
-    await sleep();
-    expect(exampleArray).toHaveLength(1);
+    await sleep(duration);
+    expect(array).toEqual(["Test Again???"]);
+  });
+
+  it("prevents the invocation when cancel is called", async () => {
+    const array: string[] = [];
+    const func = () => array.push("Test!");
+
+    debouncer.run(func, signature, duration);
+    expect(array).toEqual([]);
+
+    debouncer.cancel(signature);
+    await sleep(duration);
+    expect(array).toEqual([]);
+  });
+
+  it("runs immediately if the fireNow option is passed", async () => {
+    const array: string[] = [];
+    const func = () => array.push("Test!");
+
+    debouncer.run(func, signature, duration);
+    expect(array).toEqual([]);
+
+    debouncer.run(func, signature, duration, { fireNow: true })
+    expect(array).toEqual(["Test!"]);
   });
 });
